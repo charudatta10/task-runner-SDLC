a8ecaf7b215c3853b030b13c7c1b0ac011f3b851
charudatta10 <100494c@gmail.com>
2025-03-02 18:54:53 +0530

    Added package

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,4 @@
-import logging
+import main
 
-logging.basicConfig(
-    filename="app.log",
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
-# Copyright 2076 CHARUDATTA KORDE LLC - Apache-2.0 License
-#
-# https://raw.githubusercontent.com/github/choosealicense.com/gh-pages/_licenses/apache-2.0.txt
+
+main.main()
