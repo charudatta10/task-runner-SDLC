8cecdc022d72b23bd6187aa2898fa93983b68758
charudatta10 <100494c@gmail.com>
2025-11-10 00:04:26 +0530

    Feet: moving to poe

--- src/git.py
+++ src/git.py
@@ -1,14 +1,14 @@
 # © 2025 Charudatta Korde · Licensed under CC BY-NC-SA 4.0 · View License @ https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 
-from invoke import task, Collection
+
 from .utility import run_command
 
-@task
-def init_repo(ctx):
-    """Initialize git repo"""
-    run_command(ctx, "git init", "Git repo initialized", "Git init failed")
 
-@task
+def init_repo():
+    """Initialize git repo"""
+    run_command("git init", "Git repo initialized", "Git init failed")
+
+
 def commit_changes(ctx, message="Deployment Commit"):
     """Stage, commit, and push changes"""
     run_command(ctx, "git pull origin main", "Pulled latest changes", "Pull failed")
@@ -16,4 +16,4 @@ def commit_changes(ctx, message="Deployment Commit"):
     run_command(ctx, f'git commit -m "{message}"', "Changes committed", "Commit failed")
     run_command(ctx, "git push -u origin main", "Changes pushed", "Push failed")
 
-ns = Collection(init_repo, commit_changes)
+
